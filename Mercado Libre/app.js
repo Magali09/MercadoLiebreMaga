c92cdeb35e5d6f6fbb94368f2828fd6e9f76cb45
magalicristofori@hotmail.com <magalicristofori@hotmail.com>
2022-12-14 18:47:19 -0300

    mercadoliebre

---
+++ Mercado Libre/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require("path");
+const app = express ();
+const port =3030;
+
+app.use(express.static("public"));//nos indica donde estan los archivos publicos y estaticos
+
+app.listen (port, ( )=> console.log ("Servidor escuchando puerto"));
+
+app.get('/',(req, res) => { // '/' me especifica q es la pagina indice
+    res.sendFile(path.resolve(__dirname, './views/home.html'));//path.nos permite enviar cualquier tipo de archivo al navegador
+})
+app.get("/register", (req,res) => {
+    res.sendFile(path.resolve(__dirname + "/views/register.html"));
+});
+
+app.get("/login", (req,res) => {
+    res.sendFile(path.resolve(__dirname + "/views/login.html"));
+});
